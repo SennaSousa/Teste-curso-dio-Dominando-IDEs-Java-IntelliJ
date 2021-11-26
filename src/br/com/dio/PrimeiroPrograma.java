d3604973c4e6506d9b0611bf68d400879ddebfbf
Ayrton Senna <senna_sousa@hotmail.com>
2021-11-26 08:56:52 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,11 +5,38 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
 
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
     }
 }
+    class Livro {
+        private String nome;
+        private Integer npag;
+
+        public Livro(String nome, Integer npag) {
+            this.nome = nome;
+            this.npag = npag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNpag() {
+            return npag;
+        }
+
+        public void setNpag(Integer npag) {
+            this.npag = npag;
+        }
+    }
+
